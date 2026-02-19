d07ee19e70c29913ba4769019a71f332756b1aea
DrChungo <litingo1000@gmail.com>
2026-02-19 01:18:36 +0100

    add seeds

--- Src/Config/RngConfig.cs
+++ Src/Config/RngConfig.cs
@@ -7,6 +7,9 @@ namespace RoguelikeYago.Src.Config;
 
 public class RngConfig
 {
+    [JsonPropertyName("useSeed")]
+    public bool UseSeed { get; set; } = false;
+
     [JsonPropertyName("defaultSeed")]
     public int DefaultSeed { get; set; } = 12345;
 }
